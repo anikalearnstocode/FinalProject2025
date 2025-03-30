6267309e7e62c05cb1573c0dd6b4de21bcfe6f34
Anika Krieger <180886466+anikalearnstocode@users.noreply.github.com>
2025-03-30 16:25:55 -0500

    First iteration

---
+++ script.js
@@ -0,0 +1,62 @@
+let ripples = []; //declare ripples!
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noFill();
+
+}
+
+function draw() {
+    background(135, 206, 235, 20); //last value is for slight transparency
+
+    for (let i = ripples.length - 1; i>= 0; i--) {
+        if (ripples[i]) {
+            ripples[i].expand();
+            ripples[i].display();
+        
+
+            //remove ripples when fully faded
+            if (ripples[i].isFaded()) {
+                ripples.splice(i, 1); 
+            }
+        }
+    }
+}
+
+function mousePressed() {
+    console.log("Mouse clicked at:", mouseX, mouseY); //tester to make sure mousePressed is running
+    
+    let r = new Ripple(mouseX, mouseY);
+    console.log("Created ripple:", r); //another test!
+    ripples.push(r);
+
+}
+
+//START OF CLASS
+class Ripple {
+    //constructor
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 0; //start at 0 bc they expand
+        this.alpha = 255; //this is for opacity - it'll go down as it expands
+
+    }
+
+    expand() {
+        this.radius += 8;
+        this.alpha -= 10;
+
+    }
+
+    display() {
+        noFill();
+        stroke(255, this.alpha);
+        //strokeWeight(2);
+        ellipse(this.x, this.y, this.radius * 2);
+    }
+
+    isFaded() {
+        return this.alpha <= 0;
+    }
+}
